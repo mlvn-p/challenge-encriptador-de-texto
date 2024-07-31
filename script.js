cd41cd15057d2b17e4ee85d9c2546dc89636dc30
Malvina Palmero <malvinapalmero.dev@gmail.com>
2024-07-31 19:37:41 -0300

    Modificaciones en HTML, CSS y JavaScript

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-function encryptText() {
+function encriptarTxt() {
     const inputText = document.getElementById('IngresarTexto').value;
-    const encryptedText = inputText
+    const encriptarTxt = inputText
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
         .replace(/a/g, 'ai')
         .replace(/o/g, 'ober')
         .replace(/u/g, 'ufat');
-    document.getElementById('SalidaTexto').value = encryptedText;
-    showOutput();
+    document.getElementById('SalidaTexto').value = encriptarTxt;
+    mostrarSalida();
 }
 
-function decryptText() {
+function desencriptarTxt() {
     const inputText = document.getElementById('IngresarTexto').value;
-    const decryptedText = inputText
+    const desencriptarTxt = inputText
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ai/g, 'a')
         .replace(/ober/g, 'o')
         .replace(/ufat/g, 'u');
-    document.getElementById('SalidaTexto').value = decryptedText;
-    showOutput();
+    document.getElementById('SalidaTexto').value = desencriptarTxt;
+    mostrarSalida();
 }
 
-function copyText() {
+function copiarTxt() {
     const outputText = document.getElementById('SalidaTexto');
     outputText.select();
     document.execCommand('copy');
     alert('Texto copiado al portapapeles');
 }
 
-function showOutput() {
+function mostrarSalida() {
     document.getElementById('mensajeInicial').style.display = 'none';
     document.getElementById('mensajeDescripcion').style.display = 'none';
     document.querySelector('.imgsalida').style.display = 'none';
@@ -37,7 +37,7 @@ function showOutput() {
     document.querySelector('.botones__salida').style.display = 'flex';
 }
 
-function resetEncryptor() {
+function resetEncriptador() {
     document.getElementById('IngresarTexto').value = '';
     document.getElementById('SalidaTexto').value = '';
     document.getElementById('mensajeInicial').style.display = 'block';
